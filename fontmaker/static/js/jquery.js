7c9e9122adc50981811abd802c96972544183016
Howon <shinhw371@naver.com>
2021-08-24 11:43:26 +0900

    프로젝트 초기 팝업 화면 모양만

--- fontmaker/static/js/jquery.js
+++ fontmaker/static/js/jquery.js
@@ -1,19 +1,36 @@
 $(document).ready(function(){
-    $('.card-title').click(function(){
-        var $href = $(this).attr('href');
-        layer_popup($href);
-        console.log('jquery');
+    $('.card-title-new').click(function(){
+        let $href = $(this).attr('href');
+        layer_popup($href, 1);
+    });
+    $('.card-title-open').click(function(){
+        let $href2 = $(this).attr('href');
+        layer_popup($href2, 2);
     });
 });
 
-function layer_popup(el) {
+function layer_popup(el, num) {
 
-    var $el = $(el);    //레이어의 id를 $el 변수에 저장
-    var isDim = $el.prev().hasClass('dimBg'); //dimmed 레이어를 감지하기 위한 boolean 변수
+    // 새로 만들기 와 프로젝트 불러오기 구분용
+    let dimBg, _dimlayer, _popupclose, _layer;
+    if (num === 1) {
+        dimBg = 'dimBg';
+        _dimlayer = '.dim-layer';
+        _popupclose = '.popup-close';
+        _layer = '.layer .dimBg';
+    } else {
+        dimBg = 'dimBg2';
+        _dimlayer = '.dim-layer2';
+        _popupclose = '.popup-close2';
+        _layer = '.layer2 .dimBg2';
+    }
 
-    isDim ? $('.dim-layer').fadeIn() : $el.fadeIn();
+    let $el = $(el);    //레이어의 id를 $el 변수에 저장
+    let isDim = $el.prev().hasClass(dimBg); //dimmed 레이어를 감지하기 위한 boolean 변수
 
-    var $elWidth = ~~($el.outerWidth()),
+    isDim ? $(_dimlayer).fadeIn() : $el.fadeIn();
+
+    let $elWidth = ~~($el.outerWidth()),
         $elHeight = ~~($el.outerHeight()),
         docWidth = $(document).width(),
         docHeight = $(document).height();
@@ -28,13 +45,13 @@ function layer_popup(el) {
         $el.css({top: 0, left: 0});
     }
 
-    $el.find('a.btn-layerClose').click(function(){
-        isDim ? $('.dim-layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
+    $el.find(_popupclose).click(function(){
+        isDim ? $(_dimlayer).fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
         return false;
     });
 
-    $('.layer .dimBg').click(function(){
-        $('.dim-layer').fadeOut();
+    $(_layer).click(function(){
+        $(_dimlayer).fadeOut();
         return false;
     });
 }
